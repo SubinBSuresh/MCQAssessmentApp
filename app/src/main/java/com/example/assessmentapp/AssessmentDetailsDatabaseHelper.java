277a5dcc7d25c10438c18243079ce44208ddb2e3
SubinBSuresh <subin.bsuresh27@gmail.com>
2021-10-17 23:47:35 +0530

    Added a student dashboard, with custom listview. Added a getAssessmentList() in AssessmentDatabaseHelper

--- app/src/main/java/com/example/assessmentapp/AssessmentDetailsDatabaseHelper.java
+++ app/src/main/java/com/example/assessmentapp/AssessmentDetailsDatabaseHelper.java
@@ -2,10 +2,14 @@ package com.example.assessmentapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AssessmentDetailsDatabaseHelper extends SQLiteOpenHelper {
 
@@ -13,12 +17,12 @@ public class AssessmentDetailsDatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "ASSESSMENT_DETAILS";
     private static final int DATABASE_VERSION = 1;
 
-    public static final String KEY_ASSESSMENT_NAME = "assessment_name";
+    public static final String KEY_ASSESSMENT_NAME = "assessment_name_input";
     public static final String KEY_DUE_DATE = "due_date";
     public static final String KEY_DURATION = "duration";
 
     //CREATE TABLE
-    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "(assessment_name text, due_date text, duration text)";
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "(assessment_name_input text, due_date text, duration text)";
     //DROP TABLE
     private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
@@ -51,5 +55,23 @@ public class AssessmentDetailsDatabaseHelper extends SQLiteOpenHelper {
 
         //Insert new row in database
         db.insert(TABLE_NAME, null, cv);
+    }
+
+
+    //Retrieve Assessment details
+    public ArrayList<HashMap<String,String>> getAssessmentList(){
+        SQLiteDatabase sqLiteDatabase=this.getWritableDatabase();
+        ArrayList<HashMap<String,String>> assessmentList=new ArrayList<>();
+
+        String query="select assessment_name_input,due_date,duration from " +TABLE_NAME;
+        Cursor cursor=sqLiteDatabase.rawQuery(query,null);
+        while (cursor.moveToNext()){
+            HashMap<String,String> assessment=new HashMap<>();
+            assessment.put(KEY_ASSESSMENT_NAME,cursor.getString(0));
+            assessment.put(KEY_DUE_DATE,cursor.getString(1));
+            assessment.put(KEY_DURATION,cursor.getString(2));
+            assessmentList.add(assessment);
+        }
+        return assessmentList;
     }
 }
--- app/src/main/java/com/example/assessmentapp/StudentDashboard_11.java
+++ app/src/main/java/com/example/assessmentapp/StudentDashboard_11.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StudentDashboard_11 extends AppCompatActivity {
 
@@ -20,12 +22,25 @@ public class StudentDashboard_11 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_dashboard_11);
         //list on click move to instruction Page
-        ListView assessmentlist=findViewById(R.id.upcominglist);
+//        ListView assessmentlist=findViewById(R.id.assessmentList);
 
+        AssessmentDetailsDatabaseHelper dbHelper=new AssessmentDetailsDatabaseHelper(this);
+        ArrayList<HashMap<String,String>> AssessmentList=dbHelper.getAssessmentList();
+        ListView listView=findViewById(R.id.lv_assessmentList);
+        ListAdapter listAdapter=new SimpleAdapter(StudentDashboard_11.this,AssessmentList,R.layout.student_dashboard_listview,new String[]{"assessment_name_input","due_date","duration"},new int[]{R.id.tv_student_dashboardAssessmentName,R.id.tv_student_dashboardAssessmentDueDate,R.id.tv_student_dashboardAssessmentDuration} );
+        listView.setAdapter(listAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(StudentDashboard_11.this, InstuctionPage_8.class);
+                intent.putExtra("Assessment", position);
+                startActivity(intent);
+            }
+        });
 
         //back button for moving to student page
-        Button btnback =findViewById(R.id.btnback);
+        ImageButton btnback =findViewById(R.id.btnback);
         btnback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
